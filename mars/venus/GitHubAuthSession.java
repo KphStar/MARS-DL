42f39d9669baa5b0a34506751181dfa2b085912f
dle8604-GlazerCoder <dle8604@sdsu.edu>
2025-12-09 13:38:33 -0800

    change some note

--- mars/venus/GitHubAuthSession.java
+++ mars/venus/GitHubAuthSession.java
@@ -4,7 +4,7 @@ public final class GitHubAuthSession {
     public static volatile GitHubAuthSession CURRENT;
 
     public final String accessToken;
-    public final String login;   // GitHub username (login)
+    public final String login;  
     public final String avatarUrl;  
 
     public GitHubAuthSession(String accessToken, String login, String avatarUrl) {
