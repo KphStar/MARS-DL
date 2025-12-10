d2e262f02001a1624421702d31f96f84f6fc9e58
dle8604-GlazerCoder <dle8604@sdsu.edu>
2025-12-09 16:15:14 -0800

    change in help tab

--- mars/venus/HelpAboutAction.java
+++ mars/venus/HelpAboutAction.java
@@ -41,21 +41,28 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
          super(name, icon, descrip, mnemonic, accel, gui);
       }
    		  
-       public void actionPerformed(ActionEvent e){
-          JOptionPane.showMessageDialog(mainUI,
-			              "MARS "+Globals.version+"    Copyright "+Globals.copyrightYears+"\n"+
-							  Globals.copyrightHolders+"\n"+
-							  "MARS is the Mips Assembler and Runtime Simulator.\n\n"+
-							  "Mars image courtesy of NASA/JPL.\n"+
-							  "Toolbar and menu icons are from:\n"+
-							  "  *  Tango Desktop Project (tango.freedesktop.org),\n"+
-							  "  *  glyFX (www.glyfx.com) Common Toolbar Set,\n"+
-							  "  *  KDE-Look (www.kde-look.org) crystalline-blue-0.1,\n"+
-							  "  *  Icon-King (www.icon-king.com) Nuvola 1.0.\n"+
-							  "Print feature adapted from HardcopyWriter class in David Flanagan's\n"+
-							  "Java Examples in a Nutshell 3rd Edition, O'Reilly, ISBN 0-596-00620-9.",
-							  "About Mars",
-							  JOptionPane.INFORMATION_MESSAGE,
-							  new ImageIcon("images/RedMars50.gif"));
-      }
+      public void actionPerformed(ActionEvent e){
+    JOptionPane.showMessageDialog(
+        mainUI,
+        "MARS " + Globals.version + "    Copyright " + Globals.copyrightYears + "\n" +
+        Globals.copyrightHolders + "\n" +
+        "MARS is the Mips Assembler and Runtime Simulator.\n\n" +
+        "Mars image courtesy of NASA/JPL.\n" +
+        "Toolbar and menu icons are from:\n" +
+        "  *  Tango Desktop Project (tango.freedesktop.org),\n" +
+        "  *  glyFX (www.glyfx.com) Common Toolbar Set,\n" +
+        "  *  KDE-Look (www.kde-look.org) crystalline-blue-0.1,\n" +
+        "  *  Icon-King (www.icon-king.com) Nuvola 1.0.\n" +
+        "Print feature adapted from HardcopyWriter class in David Flanagan's\n" +
+        "Java Examples in a Nutshell 3rd Edition, O'Reilly, ISBN 0-596-00620-9.\n\n" +
+        "----- Modifications in version 4.6 -----\n" +
+        "Modified & maintained by Duc Le (KphStar)\n" +
+        "GitHub:  https://github.com/KphStar\n" +
+        "Website: https://kphstar.github.io\n",
+        "About Mars",
+        JOptionPane.INFORMATION_MESSAGE,
+        new ImageIcon("images/RedMars50.gif")
+    );
+}
+
    }
